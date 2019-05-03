900d2cec2178e0134fbdffec8fe9f458d3277f9b
Rommel Rico <me@rommelrico.com>
2019-05-03 06:58:23 -0700

    feat(test): Checking for comment box test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -6,6 +6,6 @@ it('shows a comment box', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App/>, div);
   // Look inside the div and check for the CommentBox.
-  console.log(div.innerHTML);
+  expect(div.innerHTML).toContain('Comment Box');
   ReactDOM.unmountComponentAtNode(div);
 });
