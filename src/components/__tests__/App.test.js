e5157c2c4cc3faf4b6207be8713f4b46a41d196f
Rommel Rico <me@rommelrico.com>
2019-05-03 07:02:00 -0700

    minor(testing): Adding a comment

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -6,6 +6,7 @@ it('shows a comment box', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App/>, div);
   // Look inside the div and check for the CommentBox.
+  // expect(<ThingToInspect).<MATCHER>(<ValueToExpect>)
   expect(div.innerHTML).toContain('Comment Box');
   ReactDOM.unmountComponentAtNode(div);
 });
