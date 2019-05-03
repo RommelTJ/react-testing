01b58a76fc8dd043e97622a23a397bf91203caa9
Rommel Rico <me@rommelrico.com>
2019-05-03 06:56:15 -0700

    minor(test): Console logging the div

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -6,6 +6,6 @@ it('shows a comment box', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App/>, div);
   // Look inside the div and check for the CommentBox.
-
+  console.log(div.innerHTML);
   ReactDOM.unmountComponentAtNode(div);
 });
