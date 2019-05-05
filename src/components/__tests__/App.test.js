3919e6f0f147b4fb79475cfdf083019fdb72d464
Rommel Rico <me@rommelrico.com>
2019-05-05 13:34:13 -0700

    feat(testing): Removing old jest test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -3,10 +3,5 @@ import ReactDOM from 'react-dom';
 import App from '../App';
 
 it('shows a comment box', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App/>, div);
-  // Look inside the div and check for the CommentBox.
-  // expect(<ThingToInspect).<MATCHER>(<ValueToExpect>)
-  expect(div.innerHTML).toContain('Comment Box');
-  ReactDOM.unmountComponentAtNode(div);
+  
 });
