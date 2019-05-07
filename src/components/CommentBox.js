28cacf08185d74d5082d6fa2b1349a8487cbbfff
Rommel Rico <me@rommelrico.com>
2019-05-07 07:06:37 -0700

    feat(CommentBox): adding OnSubmit handler to the form

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -8,9 +8,15 @@ class CommentBox extends Component {
     this.setState({comment: e.target.value});
   };
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: Call an action creator and save the comment.
+    this.setState({comment: ''});
+  };
+
   render() {
     return (
-      <form>
+      <form onSubmit={(e) => this.handleSubmit(e)}>
         <h4>Add a Comment</h4>
         <textarea value={this.state.comment} onChange={(e) => this.handleChange(e)}/>
         <div>
