282b2968f70651a5db794a85b98fb15b7f341d7e
Rommel Rico <me@rommelrico.com>
2019-05-07 07:04:39 -0700

    feat(CommentBox): Updating the state

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -4,11 +4,15 @@ class CommentBox extends Component {
 
   state = { comment: '' };
 
+  handleChange = (e) => {
+    this.setState({comment: e.target.value});
+  };
+
   render() {
     return (
       <form>
         <h4>Add a Comment</h4>
-        <textarea value={this.state.comment} />
+        <textarea value={this.state.comment} onChange={(e) => this.handleChange(e)}/>
         <div>
           <button>Submit Comment</button>
         </div>
