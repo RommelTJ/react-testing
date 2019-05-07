6aee350e607f7ebf90cb21baf78bd5eddeba9796
Rommel Rico <me@rommelrico.com>
2019-05-07 07:03:03 -0700

    feat(CommentBox): Adding state

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -2,11 +2,13 @@ import React, {Component} from 'react';
 
 class CommentBox extends Component {
 
+  state = { comment: '' };
+
   render() {
     return (
       <form>
         <h4>Add a Comment</h4>
-        <textarea />
+        <textarea value={this.state.comment} />
         <div>
           <button>Submit Comment</button>
         </div>
