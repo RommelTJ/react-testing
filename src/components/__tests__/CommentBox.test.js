00c85995ac3f0c385286657a83d59557058848a7
Rommel Rico <me@rommelrico.com>
2019-05-11 10:46:23 -0700

    feat(testing): Describe block

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -17,16 +17,22 @@ it('has a text area and a button', () => {
   expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a text area that users can type in', () => {
-  wrapped.find('textarea').simulate('change', {target: {value: 'new comment'}});
-  wrapped.update(); // necessary because setState is called asynchronously.
-  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+describe('the text area', () => {
+
+  it('has a text area that users can type in', () => {
+    wrapped.find('textarea').simulate('change', {target: {value: 'new comment'}});
+    wrapped.update(); // necessary because setState is called asynchronously.
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  });
+
+  it('empties textarea after submission', () => {
+    wrapped.find('textarea').simulate('change', {target: {value: 'new comment'}});
+    wrapped.update(); // necessary because setState is called asynchronously.
+    wrapped.find('form').simulate('submit');
+    wrapped.update(); // necessary because setState is called asynchronously.
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
+
 });
 
-it('empties textarea after submission', () => {
-  wrapped.find('textarea').simulate('change', {target: {value: 'new comment'}});
-  wrapped.update(); // necessary because setState is called asynchronously.
-  wrapped.find('form').simulate('submit');
-  wrapped.update(); // necessary because setState is called asynchronously.
-  expect(wrapped.find('textarea').prop('value')).toEqual('');
-});
+
