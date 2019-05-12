7ff11adaed5686a32908d781b204dab838d0a2eb
Rommel Rico <me@rommelrico.com>
2019-05-12 10:28:19 -0700

    feat(testing): Redux initial state

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -6,8 +6,9 @@ import Root from '../../Root';
 let wrapped;
 
 beforeEach(() => {
+  const initialState = { comments: ['Comment 1', 'Comment 2']};
   wrapped = mount(
-    <Root>
+    <Root initialState={initialState}>
       <CommentList/>
     </Root>
   )
