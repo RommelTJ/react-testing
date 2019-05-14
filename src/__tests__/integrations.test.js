521eb02c630f68f15889b9668ce579d50a839ea9
Rommel Rico <me@rommelrico.com>
2019-05-14 06:49:52 -0700

    fix(testing): Fixing the CommentBox test

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import Root from 'Root';
-import App from 'components/App';
+import Root from '../Root';
+import App from '../components/App';
 
 it('can fetch a list of comments and display them', () => {
   // Attempt to render the entire app.
