8d969982fedb3c0fba6702c30c44f1ac96569e11
Rommel Rico <me@rommelrico.com>
2019-05-15 21:14:47 -0700

    feat(testing): Mocking with Moxios

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -2,6 +2,22 @@ import React from 'react';
 import { mount } from 'enzyme';
 import Root from '../Root';
 import App from '../components/App';
+import moxios from 'moxios';
+
+beforeEach(() => {
+  moxios.install();
+  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [
+      {name: "Comment"},
+      {name: "Second comment"}
+    ]
+  });
+});
+
+afterEach(() => {
+  moxios.uninstall();
+});
 
 it('can fetch a list of comments and display them', () => {
   // Attempt to render the entire app.
@@ -11,5 +27,5 @@ it('can fetch a list of comments and display them', () => {
   wrapped.find('.fetch-comments').simulate('click');
 
   // Expect to find a list of comments.
-  expect(wrapped.find('li').length).toEqual(500);
+  expect(wrapped.find('li').length).toEqual(2);
 });
