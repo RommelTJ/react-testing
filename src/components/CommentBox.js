926ae040c5baba096afc0ea1caf1c06dd48a4e5c
Rommel Rico <me@rommelrico.com>
2019-05-20 06:49:57 -0700

    feat(CommentBox): Adding mapStateToProps to CommentBox

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -34,4 +34,8 @@ class CommentBox extends Component {
   }
 }
 
-export default connect(null, actions)(CommentBox);
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default connect(mapStateToProps, actions)(CommentBox);
