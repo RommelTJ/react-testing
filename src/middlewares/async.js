43d3a6b20e9c8b5aa9d394bc3dfea18b868d5811
Rommel Rico <me@rommelrico.com>
2019-05-22 07:14:29 -0700

    feat(async): Forwarding actions

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,3 +1,8 @@
 export default ({dispatch}) => (next) => (action) => {
-  // TODO: Write middleware
+  // Check to see if the action has a promise on its payload property
+  // If it does, wait to resolve
+  // If it doesn't, forward to the next middleware
+  if (!action.payload || !action.payload.then) {
+    return next(action);
+  }
 }
