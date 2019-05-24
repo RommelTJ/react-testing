e16dc7a23dc844911404573d6c9ced430c2b13f9
Rommel Rico <me@rommelrico.com>
2019-05-23 21:11:43 -0700

    feat(stateValidator): Validating with the State Schema and TV4

--- src/middlewares/stateValidator.js
+++ src/middlewares/stateValidator.js
@@ -1,3 +1,9 @@
-export default ({dispatch}) => (next) => (action) => {
+import stateSchema from "./stateSchema";
+import tv4 from "tv4";
+
+export default ({ dispatch, getState }) => (next) => (action) => {
   next(action);
-}
+  if(!tv4.validate(getState(), stateSchema)) {
+    console.warn("Invalid State Schema Detected!");
+  }
+};
