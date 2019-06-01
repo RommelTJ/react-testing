2b31aee59fbda04f38d233692d682654bc13d2e7
Rommel Rico <me@rommelrico.com>
2019-06-01 08:28:00 -0700

    feat(SignUp): Initial version

---
+++ src/components/auth/SignUp.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+
+  render() {
+    return (
+      <form>
+        <fieldset>
+          <label>Email</label>
+        </fieldset>
+        <fieldset>
+          <label>Password</label>
+        </fieldset>
+      </form>
+    );
+  }
+
+}
+
+export default SignUp;
