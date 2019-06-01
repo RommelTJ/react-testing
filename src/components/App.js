7cee89b571efd8aedfee0ff243ef06385ab4193c
Rommel Rico <me@rommelrico.com>
2019-06-01 08:09:35 -0700

    feat(App): Adding the Header to the App

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,17 @@
 import React, {Component} from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import CommentBox from './CommentBox';
 import CommentList from './CommentList';
 import * as actions from "../actions";
+import Header from "./Header";
 
 class App extends Component {
-
-  renderLogInButton() {
-    if (this.props.auth) {
-      return <button onClick={() => this.props.changeAuth(false)}>Sign Out</button>;
-    }
-    return <button onClick={() => this.props.changeAuth(true)}>Sign In</button>
-  }
-
-  renderHeader() {
-    return (
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/post">Post a Comment</Link>
-        </li>
-        <li>{ this.renderLogInButton() }</li>
-      </ul>
-    );
-  }
 
   render() {
     return (
       <div>
-        {this.renderHeader()}
+        <Header auth={this.props.auth} changeAuth={this.props.changeAuth} />
         <Route path="/post" component={CommentBox} />
         <Route path="/" exact component={CommentList} />
       </div>
