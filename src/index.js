7518af96b53c36df42bc2f3becbf456e5e466cf8
Rommel Rico <me@rommelrico.com>
2019-06-01 08:28:11 -0700

    feat(index): Adding a route to sign up

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Root from './Root';
 import App from './components/App';
 import Welcome from './components/Welcome';
+import SignUp from "./components/auth/SignUp";
 
 ReactDOM.render(
   <Root>
     <BrowserRouter>
       <App>
         <Route path="/" exact component={Welcome} />
+        <Route path="/signup" exact component={SignUp} />
       </App>
     </BrowserRouter>
   </Root>,
