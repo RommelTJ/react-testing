6326a911fca852a7d3456ff24860c6611f1724d6
Rommel Rico <me@rommelrico.com>
2019-06-03 07:07:09 -0700

    feat(Root): Wiring up reduxThunk

--- src/Root.js
+++ src/Root.js
@@ -3,9 +3,10 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import async from "./middlewares/async";
 import reducers from './reducers';
+import reduxThunk from 'redux-thunk';
 
 export default ({children, initialState = {}}) => {
-  const store = createStore(reducers, initialState, applyMiddleware(async));
+  const store = createStore(reducers, initialState, applyMiddleware(async, reduxThunk));
   return (
     <Provider store={store}>
       {children}
