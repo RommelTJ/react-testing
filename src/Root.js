143d644c5f8fd94175e33cfe724ef52fb69a2eb6
Rommel Rico <me@rommelrico.com>
2019-06-03 06:59:40 -0700

    fix(Root): Removing the State Validator middleware

--- src/Root.js
+++ src/Root.js
@@ -3,10 +3,9 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import async from "./middlewares/async";
 import reducers from './reducers';
-import stateValidator from "./middlewares/stateValidator";
 
 export default ({children, initialState = {}}) => {
-  const store = createStore(reducers, initialState, applyMiddleware(async, stateValidator));
+  const store = createStore(reducers, initialState, applyMiddleware(async));
   return (
     <Provider store={store}>
       {children}
